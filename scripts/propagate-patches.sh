09cb972bc1accf8c0db86a7da6dd8ac99874d9f0
bartoszmajsak <bartosz.majsak@gmail.com>
2022-03-30 19:12:16 +0200

    feat(script): propagates patches on default branch change

--- scripts/propagate-patches.sh
+++ scripts/propagate-patches.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 set -euo pipefail
@@ -7,6 +8,7 @@ set -euo pipefail
 # shellcheck disable=SC1091
 source "${DIR}/func.sh"
 dryRun=false
+skipPr=false
 
 PULL_BASE_REF="${PULL_BASE_REF:-}"
 if [[ -z $PULL_BASE_REF ]]; then
@@ -16,6 +18,7 @@ fi
 previous_branch="${PULL_BASE_REF%:*}"
 current_branch="${PULL_BASE_REF#*:}"
 patchset_repo="${PATCHSET_REPO:-}" # required
+source_repo="${SOURCE_REPO:-}" # required
 patchset_dir="${PATCHSET_DIR:-patchset}"
 
 gh_token="null"
@@ -47,12 +50,20 @@ while test $# -gt 0; do
             patchset_repo=$(echo $1 | sed -e 's/^[^=]*=//g')
             shift
             ;;
+    --repo*)
+            source_repo=$(echo $1 | sed -e 's/^[^=]*=//g')
+            shift
+            ;;
     --token*)
             gh_token=$(echo $1 | sed -e 's/^[^=]*=//g')
             shift
             ;;
     --dry-run)
             dryRun=true
+            shift
+            ;;
+    --skip-pr)
+            skipPr=true
             shift
             ;;
     *)
@@ -78,17 +89,121 @@ patchset_dir=$(pwd)/patchset
 git clone "https://oauth2:${GITHUB_TOKEN}@${patchset_repo}.git" "${patchset_dir}"
 configure_git "${patchset_dir}"
 
+mkdir source_repo
+source_repo_dir=$(pwd)/source_repo
+
+git clone "https://oauth2:${GITHUB_TOKEN}@${source_repo}.git" "${source_repo_dir}"
+configure_git "${source_repo_dir}"
+
 cd "${patchset_dir}"
 mkdir -p "${current_branch}" "${previous_branch}"
 
 cd "${previous_branch}"
 cp -R . "../${current_branch}/"
 
+### Patchset migration
+
 cd "${patchset_dir}"
 if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -am"feat: migrates patchset from ${previous_branch} to ${current_branch}"
   skipInDryRun git push
-else
-  die "Nothing to commit. Are you sure '${previous_branch}' is the right source branch?"
 fi
+
+cd "${source_repo_dir}"
+
+## Start patch migration process
+git switch "${current_branch}"
+
+MAIN_REF=$(git rev-parse HEAD)
+
+patch_branch="patch_update_${ID}"
+patch_head="head_${ID}"
+
+skipInDryRun git branch "${patch_head}"
+skipInDryRun git branch "${patch_branch}"
+skipInDryRun git switch "${patch_branch}"
+
+for patch in "${patchset_dir}/${current_branch}/"*.patch
+do
+    [[ -e "${patch}" ]] || break  # handle the case of no *.patch files
+    patch_name=$(basename "${patch}")
+    patch_raw_url="https://${patchset_repo}/blob/main/${current_branch}/${patch_name}?raw=true"
+    set +e ## turn off exit on error to capture git am failure.. any other way?
+    echo "Applying ${patch}"
+    apply_status="$(git am "${patch}" -k -3 2>&1)"
+    git_am_exit=$?
+    set -e
+
+    if [ $git_am_exit -ne 0 ];
+    then
+        err_diff=$(git am --show-current-patch=diff)
+        skipInDryRun git am --abort
+        skipInDryRun git push origin "${patch_branch}"
+
+        skipInDryRun git switch "${patch_head}"
+        skipInDryRun git push origin "${patch_head}"
+
+        skipInDryRun git switch "${patch_branch}"
+
+        skipInDryRun gh api --silent repos/"${source_repo#*/}"/labels -f name="do-not-merge" -f color="E11218" || true
+
+        if ! $skipPr; then
+                echo ">> test"
+                prOutput=$(skipInDryRun gh pr create \
+                        --base "${patch_head}" \
+                        --head  "${patch_branch}" \
+                        --title "fix: resolving patchset on ${current_branch}@${MAIN_REF}" \
+                        --label "do-not-merge" \
+                        --body-file - << EOF
+## Why this PR?
+
+This pull request is indented for resolving conflicts in patchset between \`${previous_branch}\` and changes done on ongoing development branch \`${current_branch}\`.
+
+### Resolving the conflict
+
+Apply the patch from the patchset repository
+
+\`\`\`
+git checkout ${patch_branch}
+curl -L ${patch_raw_url}  | git am -k -3
+\`\`\`
+
+resolve the conflict and push back to the branch as a single commit.
+
+### Next steps
+
+Now you can continue verification process by invoking one of the commands:
+
+ * \`/test\` will run unit tests
+ * \`/lint\` will run perform lint checks on the code
+ * \`/resolved\` will update the patch in the patchset and continue verification process if there are more patches.
+
+You can find all the relevant patches in [patchset](https://${patchset_repo}/tree/main/${current_branch}) repository.
+
+## Details
+
+### Message
+\`\`\`
+${apply_status}
+\`\`\`
+### Conflict
+\`\`\`diff
+${err_diff}
+\`\`\`
+
+EOF
+)
+
+                pr_nr=$(echo "${prOutput}" | grep -oP "pull/\K.*" || echo "0")
+                patch_label="patch/${current_branch}/${patch_name%%-*}"
+                repo_slug="${source_repo#*/}"
+                skipInDryRun gh api --silent repos/"${repo_slug}"/labels -f name="${patch_label}" -f color="c0ff00" || true
+                skipInDryRun gh api --silent --method POST repos/"${repo_slug}"/issues/"${pr_nr}"/labels --input - <<EOF
+{ "labels": ["${patch_label}"] }
+EOF
+        fi
+        exit $git_am_exit # is there a distinction between failed and errored job?
+    fi
+
+done
